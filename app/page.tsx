71d37a9fdeaace2ba8a91e560319cd407ad1b408
sofi-knots <romeshjjames@gmail.com>
2026-03-22 23:28:19 +0530

    fix: make homepage reflect admin edits

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import { getCatalogPageBySlug } from "@/lib/catalog";
 import { buildStorefrontMetadata } from "@/lib/seo";
 import { getStorefrontSettings } from "@/lib/storefront";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function generateMetadata(): Promise<Metadata> {
   const [homePageResult, storefront] = await Promise.all([getCatalogPageBySlug("home"), getStorefrontSettings()]);
   const homePage = homePageResult.data;
